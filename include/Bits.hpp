98cefba57f2352396b2d891843fd4555ac21f686
Joshua Lawrence Austill <jlaustill@gmail.com>
2024-08-31 12:28:57 -0600

    make bits generic to avoid overflows with larger types

--- include/Bits.hpp
+++ include/Bits.hpp
@@ -1,18 +1,52 @@
 #ifndef SEADASH_BITUTILS_HPP
 #define SEADASH_BITUTILS_HPP
+#include <math.h>
+
+#include <climits>
 
 namespace SeaDash {
 namespace Bits {
 
-unsigned getNOnes(unsigned n);
+template <typename T>
+T getNOnes(unsigned n) {
+  return (static_cast<T>(1) << n) - 1;
+}
 
-unsigned getNOnesAt(unsigned n, unsigned offset);
+template <typename T>
+T getNOnesAt(unsigned n, unsigned offset) {
+  return getNOnes<T>(n) << offset;
+}
 
-unsigned getMaxN();
+template <typename T>
+unsigned getMaxN() {
+  return CHAR_BIT * sizeof(T);
+}
 
-unsigned getNBits(unsigned value, unsigned offset, unsigned n);
+template <typename T>
+T getNBits(T value, unsigned offset, unsigned n) {
+  const unsigned max_n = getMaxN<T>();
+  if (offset >= max_n)
+    return 0;       /* value is padded with infinite zeros on the left */
+  value >>= offset; /* drop offset bits */
+  if (n >= max_n) return value;         /* all  bits requested */
+  const unsigned mask = getNOnes<T>(n); /* n '1's */
+  return value & mask;
+}
 
-unsigned setNBitsAt(unsigned value, unsigned bits, unsigned offset, unsigned n);
+template <typename T>
+T setNBitsAt(T value, T bits, unsigned offset, unsigned n) {
+  const unsigned max_n = getMaxN<T>();
+  // first we check for in bounds offset
+  if (offset >= max_n || offset + n >= max_n) {
+    return value;  // offset is out of bounds
+  }
+  bits = getNBits(bits, 0, n);  // Make sure not to set too many bits
+  // first we need to zero out the bits we are going to set
+  value &= ~getNOnesAt<T>(n, offset);
+  value |= bits << n;
+
+  return value;
+}
 }  // namespace Bits
 }  // namespace SeaDash
 
--- test/Bit_test.cpp
+++ test/Bit_test.cpp
@@ -5,48 +5,48 @@
 
 // Demonstrate some basic assertions.
 TEST(BitUtilsTest, getNBitsWorks) {
-  uint8_t result = SeaDash::Bits::getNBits(0b11100000, 5, 3);
+  uint8_t result = SeaDash::Bits::getNBits<uint8_t>(0b11100000, 5, 3);
   // Expect equality.
   EXPECT_EQ(result, 0b111);
 
-  result = SeaDash::Bits::getNBits(0b10101101, 2, 4);
+  result = SeaDash::Bits::getNBits<uint8_t>(0b10101101, 2, 4);
   EXPECT_EQ(result, 0b1011);
 }
 
 TEST(BitUtilsTest, getNBitsHandlesOutOfBounds) {
-  uint8_t result = SeaDash::Bits::getNBits(0b1111, 999, 1);
+  uint8_t result = SeaDash::Bits::getNBits<uint32_t>(0b1111, 999, 1);
   EXPECT_EQ(result, 0);
 
-  result = SeaDash::Bits::getNBits(0b1111, 0, 50000000);
+  result = SeaDash::Bits::getNBits<uint32_t>(0b1111, 0, 50000000);
   EXPECT_EQ(result, 0b1111);
 }
 
 TEST(BitUtilsTest, getNOnes) {
-  uint32_t result = SeaDash::Bits::getNOnes(4);
+  uint32_t result = SeaDash::Bits::getNOnes<uint32_t>(4);
 
   EXPECT_EQ(result, 0b1111);
 }
 
 TEST(BitUtilsTest, getNOnesAt) {
-  uint32_t result = SeaDash::Bits::getNOnesAt(4, 4);
+  uint32_t result = SeaDash::Bits::getNOnesAt<uint32_t>(4, 4);
 
   EXPECT_EQ(result, 0b11110000);
 }
 
 TEST(BitUtilsTest, setNBits) {
-  uint32_t result = SeaDash::Bits::setNBitsAt(0b0001, 0b11, 2, 2);
+  uint32_t result = SeaDash::Bits::setNBitsAt<uint32_t>(0b0001, 0b11, 2, 2);
 
   EXPECT_EQ(result, 0b1101);
 }
 
 TEST(BitUtilsTest, setNBitsAtOutOfBounds) {
-  uint32_t result = SeaDash::Bits::setNBitsAt(0b01, 0b11111111, 999, 2);
+  uint32_t result = SeaDash::Bits::setNBitsAt<uint32_t>(0b01, 0b11111111, 999, 2);
 
   EXPECT_EQ(result, 0b01);
 }
 
 TEST(BitUtilsTest, setNBitsAtConstrainBits) {
-  uint32_t result = SeaDash::Bits::setNBitsAt(0b01, 0b111111, 1, 1);
+  uint32_t result = SeaDash::Bits::setNBitsAt<uint32_t>(0b01, 0b111111, 1, 1);
 
   EXPECT_EQ(result, 0b11);
 }
